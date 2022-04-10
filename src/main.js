8a12bfe7fe62e52cb0029c1abe24543b7b33fcaf
TechnoMasr <ezatellozy1@gmail.com>
2022-04-10 14:17:29 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,31 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import i18n from './i18n'
+import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
-createApp(App).use(store).use(router).mount("#app");
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import 'bootstrap'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas)
+library.add(fab)
+library.add(far)
+
+const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.use(BootstrapVue3)
+app.use(i18n)
+app.use(store)
+app.use(router)
+app.mount('#app')
